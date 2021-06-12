4b10036f653eaa7d83866186c5cf19f5b7c3aa6f
Binit Kumar <kumar.binit1992@gmail.com>
2021-06-12 10:01:13 -0400

    Initial Commit

---
+++ asg-1/src/ca/concordia/dsd/client/RunClient.java
@@ -0,0 +1,8 @@
+package ca.concordia.dsd.client;
+
+public class RunClient {
+
+    public static void main(String[] args) {
+        System.out.println("Run client");
+    }
+}
---
+++ asg-1/src/ca/concordia/dsd/server/RunAllServers.java
@@ -0,0 +1,7 @@
+package ca.concordia.dsd.server;
+
+public class RunAllServers {
+    public static void main(String[] args) {
+        System.out.println("Starting all servers");
+    }
+}
